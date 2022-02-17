cd664e5a18e01ec23292500877c5fbd79dc3622d
vicjo <vicjou8@gmail.com>
2022-02-17 00:49:28 -0800

    Takes in -a avatar and updated usage

--- discordwb.bat
+++ discordwb.bat
@@ -1,17 +1,22 @@
 @echo off
-
 SET WEBHOOK_URL=
 SET content=
 SET username=
+SET avatar=
 
 if "%1"=="" goto usage
 :while
 if "%1"=="" goto send
+if "%1"=="-?" goto usage
+if "%1"=="-h" goto usage
 if "%1"=="-m" (
     SET content=%2
 )
 if "%1"=="-n" (
     SET username=%2
+)
+if "%1"=="-a" (
+    SET avatar=%2
 )
 shift
 shift
@@ -19,9 +24,14 @@ goto while
 
 :send
 if %content%=="" goto usage
-curl -H "Content-Type: application/json" -d "{\"username\": \"%username%\", \"content\":\"%content%\"}" %WEBHOOK_URL%
+curl -H "Content-Type: application/json" -d "{\"username\":\"%username%\", \"content\":\"%content%\", \"avatar_url\":\"%avatar%\"}" %WEBHOOK_URL%
 goto :eof
 
 :usage
-echo needs params
+echo Params
+echo REQUIRED
+echo -m: The message body
+echo OPTIONAL
+echo -n: The name of the webhook
+echo -a: The avatar of the webhook
 exit /B 1
