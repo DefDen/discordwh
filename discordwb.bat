fa5e1969b4a5887357a7b4f8dac645253154561b
vicjo <vicjou8@gmail.com>
2022-02-17 00:03:31 -0800

    Added param checking

--- discordwb.bat
+++ discordwb.bat
@@ -1,3 +1,13 @@
-SET WEBHOOK_URL=
+@echo off
+
+SET WEBHOOK_URL=https://discord.com/api/webhooks/854163170812624936/5DJ8e5o0S_tmjOlr3Ne8SC-TD-wRDUgo_pV5TfPLnl5hrUtWUAzdk3FmGKtGSUibXiig
+
+:while
+if [%1]==[] goto usage
 
 curl -H "Content-Type: application/json" -d "{\"username\": \"%2\", \"content\":\"%1\"}" %WEBHOOK_URL%
+goto :eof
+
+:usage
+echo needs params
+exit /B 1
